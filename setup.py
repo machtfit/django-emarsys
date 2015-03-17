08e0d99951d16bbccf57ff05d4dc9a52caed4c79
Jonathan Fiebelkorn <jonathan@mailbox.org>
2015-03-17 13:04:33 +0100

    Version 0.13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='django_emarsys',
-      version='0.12',
+      version='0.13',
       description='Django and Oscar glue for Emarsys events',
       license="MIT",
       author='Markus Bertheau',
