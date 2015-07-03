690ca3210ee0fdc06ca895015d11eba58980cb2a
Markus Bertheau <mbertheau@gmail.com>
2015-07-03 05:39:28 +0200

    Set up sandbox project to use django_emarsys
    so that we can use it to create migrations for django_emarsys' models.

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_emarsys',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMARSYS_ACCOUNT = 'company001'
+EMARSYS_PASSWORD = 'secret'
+EMARSYS_BASE_URI = 'https://suiteN.emarsys.net'
+EMARSYS_USE_NULL_GENERIC_CONTEXT_PROVIDER = True
