f5321c1ee99944deba1d58074129f16254f4c861
Markus Bertheau <mbertheau@gmail.com>
2016-11-09 19:56:44 +0100

    Version 0.29

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='django_emarsys',
-      version='0.28',
+      version='0.29',
       description='Django and Oscar glue for Emarsys events',
       license="MIT",
       author='Markus Bertheau',
