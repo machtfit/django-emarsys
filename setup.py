1d23d41bf230a0e8a07d1f3349bde459dda221d7
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2019-02-07 14:23:36 +0100

    Bump version to 0.41

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='django_emarsys',
-      version='0.40',
+      version='0.41',
       description='Django glue for Emarsys events',
       license="MIT",
       author='Markus Bertheau',
