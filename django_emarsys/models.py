20a3e422dbfe5587e0f09c200a851c413dc70e4f
Tim Kleinschmdit <tim.kleinschmidt@gmail.com>
2019-06-28 15:22:11 +0200

    Remove python_2_unicode_compatible use

--- django_emarsys/models.py
+++ django_emarsys/models.py
@@ -3,7 +3,6 @@ import logging
 from jsonfield import JSONField
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from django_emarsys import EventParam
@@ -11,7 +10,6 @@ from django_emarsys import EventParam
 log = logging.getLogger(__name__)
 
 
-@python_2_unicode_compatible
 class Event(models.Model):
     name = models.CharField(max_length=1024, unique=True)
     emarsys_id = models.IntegerField()
@@ -29,7 +27,6 @@ class Event(models.Model):
         return self.name
 
 
-@python_2_unicode_compatible
 class EventInstance(models.Model):
     STATE_SENDING = "sending"
     STATE_ERROR = "error"
